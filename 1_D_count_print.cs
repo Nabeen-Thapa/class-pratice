96b5a97a94339484d48792b189d68c2ab9a5e421
Nabin Thapa <151662784+Nabeen-Thapa@users.noreply.github.com>
2024-08-26 11:27:58 +0545

    Add files via upload

---
+++ 1_D_count_print.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace class_pratice
+{
+    internal class _1_D_count_print
+    {
+        public static void Main(string[] args)
+        {
+            int[] arr = { 12, 2, 3, 6, 4, 10, 15, 8 };
+            int count = arr.Length;
+            int sum = 0;
+            for (int i = 0; i < count; i++)
+            {
+                if (arr[i] % 2 == 0)
+                {
+                    sum += arr[i];
+                }
+            }
+            Console.WriteLine("here are " + count + " elements");
+            Console.WriteLine("the sum of even is " + sum);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ minmax.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace class_pratice
+{
+    internal class minmax
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("enter he size of array:");
+            int size = int.Parse(Console.ReadLine());
+            int []array = new int[size];
+          
+            Console.WriteLine("enter hte array elements:");
+            for (int i = 0; i < size; i++)
+            {
+                array[i]=int.Parse(Console.ReadLine());
+                int min = array[0];
+                int max = array[0];
+                for (i = 0; i < array.Length; ++i)
+                {
+                    if (min > array[i])
+                    {
+                        min = array[i];
+                    }
+                    if (max<array[i]) 
+                    {
+                        max=array[i];
+                    }
+                }
+                Console.WriteLine("min is: " + min);
+                Console.WriteLine("max is: " + max);
+             }
+            Console.ReadKey();
+        }
+    }
+}
